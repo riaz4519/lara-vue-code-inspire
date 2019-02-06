b35ef5175d99f89165d58f97ed6dbe1a59b1b30a
unknown <riaz.i3216@gmail.com>
2019-02-06 20:05:35 +0600

    after finishing the get

---
+++ resources/js/components/TaskForm.vue
@@ -0,0 +1,66 @@
+<template>
+
+            <div class="col-md-8 mt-4">
+                <div class="card">
+                    <div class="card-header"> Task Form</div>
+
+                    <div class="card-body">
+
+                        <form action="./api/task" method="post" @submit.prevent="addTask()">
+
+                            <div class="form-group">
+
+                                <input type="text" name="title" v-model="title" placeholder="Task title" class="form-control">
+
+                            </div>
+
+
+                            <div class="form-group">
+
+                                <input type="submit" value="Add Task" class="btn btn-info">
+
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+</template>
+
+<script>
+    export default {
+
+
+        data(){
+
+            return {
+
+                title:'',
+
+            }
+        },
+        
+        methods:{
+
+            addTask:function () {
+
+                axios.post('./api/task',{
+
+                    title: this.title,
+
+                });
+
+
+
+
+
+
+
+            }
+            
+        }
+
+    }
+</script>
---
+++ resources/js/components/TaskList.vue
@@ -0,0 +1,49 @@
+<template>
+
+            <div class="col-md-8 mt-4">
+                <div class="card">
+                    <div class="card-header"> Task List</div>
+
+                    <div class="card-body">
+
+                        <ul>
+                            <li v-for="task in tasks" :key="task.id">{{ task.title}}</li>
+                        </ul>
+
+
+
+                    </div>
+                </div>
+            </div>
+
+</template>
+
+<script>
+    export default {
+
+
+        data(){
+
+            return {
+
+                tasks:[],
+
+            }
+        },
+
+        methods:{
+
+
+        },
+
+        created(){
+
+            axios.get('./api/task').then( response => this.tasks = response.data)
+
+
+        },
+        
+
+
+    }
+</script>
